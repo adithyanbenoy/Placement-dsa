2b85dea9d2bab5a254834371a25d8752cb6ee748
adithyanbenoy <adithyanbenoy61@gmai.com>
2026-09-09 14:42:15 +0530

    Day 01 - commit 1

--- Day01/Program_02.java
+++ Day01/Program_02.java
@@ -2,9 +2,20 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         int[] arr=new int[nums.length];
+        int sum=0;
         for(int i=0;i<nums.length;i++){
             if(i==1){
-                
+                arr[i]=nums[i];
+                sum=nums[i]+nums[i+1];
+
+
+            }
+            else if(i<(nums.length-1)){
+                arr[i]=sum;
+                sum=sum+arr[i+1];
+            }
+            else{
+                arr[i]=sum;
             }
             else if(i<(nums.length-1)){
                 arr[i]=sum;
