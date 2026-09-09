0a19d4a8142cbb43941484b6973b40b5b57a3c44
adithyanbenoy <adithyanbenoy61@gmai.com>
2026-09-09 14:38:23 +0530

    update changes

---
+++ Day01/Program_02.java
@@ -0,0 +1,15 @@
+/*1480. Running Sum of 1d Array*/
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(i==1){
+                
+            }
+
+            
+
+        }
+        
+    }
+}
